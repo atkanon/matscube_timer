bc850f4631c841a360b0a3a5e5e2edcd1ade095d
TakanoriMatsumoto <matscube@TakanoriMatsumoto-no-MacBook-Pro-2.local>
2014-05-05 04:16:32 +0900

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MatscubeTimer::Application.config.secret_key_base = '11529861c193f3eab2652057179eac55495b30b610106a26a575d29d6f0922074d08ef8f1eade124eec2e739e9006f9e908472c478b51b0c364b9f3e355d39aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MatscubeTimer::Application.config.session_store :cookie_store, key: '_matscube_timer_session'
