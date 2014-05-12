28a25df902ab4b06e76dec4281312b3ff993bf9f
TakanoriMatsumoto <matscube@TakanoriMatsumoto-no-MacBook-Pro-2.local>
2014-05-12 23:56:03 +0900

    [WIP]

--- app/assets/javascripts/custom/products.js
+++ app/assets/javascripts/custom/products.js
@@ -1,37 +1,52 @@
-// Whole Application module
 var AppModule = angular.module('App', []);
 
-AppModule.controller('AppController', function($scope) {
+AppModule.controller('AppController', function($scope, $timeout) {
+	// Timerの制御
+	$scope.timer = {
+		status: 'off'
+	};
+
 	$scope.KeyUp = function(keyEvent) {
-		console.log('keyup', keyEvent.keyCode);
-	};
-	$scope.KeyDown = function(keyEvent) {
-		console.log('keydown', keyEvent.keyCode);
-	};
-
-	// Rubik's cube timer module
-	var RubikModule = angular.module('Rubik', []);
-
-	RubikModule.controller('TimerController', function($scope, $timeout) {
-		$scope.time = {
-			counter: 0,
-			minutes: "00",
-			seconds: "00",
-			milliseconds: "00" };
-
-		$scope.onTimeout = function() {
-			var counter = $scope.time.counter++;
-
-			$scope.time.milliseconds = number_format(counter % 100, 2);
-			$scope.time.seconds = number_format(Math.floor(counter / 100) % 60, 2);
-			$scope.time.minutes = number_format(Math.floor(counter / 100 / 60) % 10, 2);
-
-			$timeout($scope.onTimeout, 10);
-		};
-
-		function number_format(number, digit) {
-			return ('000' + number).slice(-digit);
+		if (keyEvent.keyCode == 32 && $scope.timer.status == 'off') {
+			$scope.timer.status = 'on';
+			$scope.onTimeout();
 		}
-	});
+		if ($scope.timer.status == 'finished') {
+			$scope.timer.status = 'off';
+		}
+	};
+
+	$scope.KeyDown = function(keyEvent) {
+		if ($scope.timer.status == 'on') {
+			$scope.timer.status = 'finished';
+		}
+		if (keyEvent.keyCode == 32 && $scope.timer.status == 'off') {
+			// ready
+		}
+	};
+
+	$scope.time = {
+		counter: 0,
+		minutes: "00",
+		seconds: "00",
+		milliseconds: "00"
+	};
+
+	$scope.onTimeout = function() {
+		var counter = $scope.time.counter++;
+
+		$scope.time.milliseconds = number_format(counter % 100, 2);
+		$scope.time.seconds = number_format(Math.floor(counter / 100) % 60, 2);
+		$scope.time.minutes = number_format(Math.floor(counter / 100 / 60) % 10, 2);
+
+		if ($scope.timer.status == 'on') {
+			$timeout($scope.onTimeout, 10);
+		}
+	};
+
+	function number_format(number, digit) {
+		return ('000' + number).slice(-digit);
+	}
+
 });
 
